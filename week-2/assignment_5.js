0c908b760f58ef7e1cdfd373df706a25b2c640b8
Po Lien Lin <polien.joe.lin@gmail.com>
2023-09-09 09:44:27 +0800

    Improve answer of week2 assignment5

--- week-2/assignment_5.js
+++ week-2/assignment_5.js
@@ -3,38 +3,75 @@ Assignment 5: Algorithm (Advanced Optional)
 Given an array of integers, return indices of the two numbers such that they add up to a
 specific target. You may assume that each input would have exactly one solution, and you
 may not use the same element twice.
+
 function twoSum(nums, target) {
 // your code here
 } 
 
-For example:
-twoSum([2, 7, 11, 15], 9);
-Should returns:
-[0, 1]
-Because:
-nums[0] + nums[1] is 9
 */
 
-/* 以下是暴力解法 */
-/* time complexity = o^n */
+/**  以下是利用 object map 的解法 */
+//   Time complexity = O(n)
 
-const twoSum = (nums, target) => {
+function twoSum(nums, target) {
 
-    let indice = [];
+    const numMap = {};
+    let indices = []
     let found = false;
 
-    nums.forEach((currentValue, index, array) => {
+    nums.forEach( (value, index) => {
+
+        let complement = target - value;
+        
+        if (found){
+            return;
+        } else if (complement in numMap) {
+            indices.push(numMap[complement], index);
+            found = true;
+        } else {
+            numMap[value] = index;
+        }
+    })
+
+    if (found){
+        return indices;
+    } else {
+        throw Error("It is not possible to add up numbers in array to the target.");
+    }
+}
+
+twoSum([2, 7, 11, 15], 9);
+twoSum([0, 3, 3, 3], 6);
+twoSum([-9, 6, 2, 0], -9);
+twoSum([1, 2, 3, 4], 10);
+
+
+
+/**  以下是暴力解法 */
+//   Time complexity = O(n^2)
+
+const twoSumBruteForce = (nums, target) => {
+
+    let indices = [];
+    let found = false;
+
+    nums.forEach( (currentValue, index, array) => {
         array.forEach( (cur, ind) => {
-            if (currentValue + cur === target && found===false ){
-                indice.push(index, ind);
-                // console.log(indice);
+            if ( index !== ind && (currentValue + cur === target) && found===false ) {
+                indices.push(index, ind);
                 found = true;
             }
         })
     })
-    return indice;
+
+    if (found){
+        return indices;
+    } else {
+        throw Error("It is not possible to add up numbers in array to the target.");
+    }
 }
 
-twoSum([2, 7, 11, 15], 9);
-
-console.log(twoSum([2, 7, 11, 15], 9));
+twoSumBruteForce([2, 7, 11, 15], 9);
+twoSumBruteForce([0, 3, 3, 3], 6);
+twoSumBruteForce([-9, 6, 2, 0], -9);
+twoSumBruteForce([1, 2, 3, 4], 10);
