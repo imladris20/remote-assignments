d1895e3cb00e018a4cd20e8e0a68c7ad623bf4ff
Po Lien Lin <polien.joe.lin@gmail.com>
2023-09-09 12:28:11 +0800

    Finish building request 2 of week2 assignment4

--- week-2/assignment_4/script/script.js
+++ week-2/assignment_4/script/script.js
@@ -8,3 +8,43 @@ welcomeBlock.addEventListener('click', ()=> {
 })
 
 // Request 2
+const origin = [
+    document.querySelector('.main-logo'),
+    document.querySelector('.menu'),
+]
+
+const float = [
+    document.querySelector('.close-logo'),
+    document.querySelector('.close'),
+    document.querySelector('.main-nav-float-ul'),
+]
+
+for (let item of document.querySelectorAll('.main-nav-float-ul a')) {
+    float.push(item);
+}
+
+origin[0].addEventListener('click', () => {
+
+    for (let node of origin) {
+        node.style.display = 'none';
+    }
+
+    for (let node of float) {
+        node.style.display = 'block';
+    }
+
+});
+
+float[0].addEventListener('click', () => {
+
+    for (let node of origin) {
+        node.style.display = '';
+    }
+
+    for (let node of float) {
+        node.style.display = 'none';
+    }
+
+})
+
+// Request 3
