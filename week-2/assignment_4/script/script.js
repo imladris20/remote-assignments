57c7ded60d284aee56eb1fa93241d948078f2851
Po Lien Lin <polien.joe.lin@gmail.com>
2023-09-09 10:25:12 +0800

    Finish request1 of week2 assignment4

---
+++ week-2/assignment_4/script/script.js
@@ -0,0 +1,10 @@
+// Request 1
+
+const welcomeBlock = document.querySelector('.message');
+const welcomeMessage = document.querySelector('.welcome-message');
+
+welcomeBlock.addEventListener('click', ()=> {
+    welcomeMessage.textContent = "一起面對這座島嶼的創傷";
+})
+
+// Request 2
