3e70eba6dc7068ad90662ccebe94015e27125a2e
Po Lien Lin <polien.joe.lin@gmail.com>
2023-09-09 15:56:36 +0800

    Finish build request3 of week2 assignment4

--- week-2/assignment_4/script/script.js
+++ week-2/assignment_4/script/script.js
@@ -5,7 +5,7 @@
 const welcomeBlock = document.querySelector('.message');
 const welcomeMessage = document.querySelector('.welcome-message');
 
-welcomeBlock.addEventListener('click', ()=> {
+welcomeBlock.addEventListener('click', () => {
     welcomeMessage.textContent = "一起面對這座島嶼的創傷";
 });
 
@@ -51,3 +51,44 @@ float[0].addEventListener('click', () => {
 /* -------------------------------------
      Request 3
 --------------------------------------*/
+
+const btnMoreContent = document.querySelector('.more-content');
+const btnClickHide = document.querySelector('.click-hide-a');
+const allContent = document.querySelectorAll('.content');
+const hide = document.querySelectorAll('.hide');
+const hideContent = document.querySelectorAll('.hide-content')
+const btnContent = document.querySelectorAll('.content .button');
+let isClicked = false;
+
+btnMoreContent.addEventListener('click', () => {
+    if (!isClicked) {
+        for (let node of allContent) {
+            node.style.display = 'flex';
+            node.style.flex = '1';
+            node.style.flexDirection = 'column';
+            node.style.flexBasis = '40%';
+            node.style.justifyContent = 'space-between';
+            node.style.alignItems = 'center';
+        }
+    
+        for (let node of hideContent) {
+            node.style.display = 'block';
+        }
+    
+        for (let node of btnContent) {
+            node.style.marginTop = 'auto';
+            node.style.alignSelf = 'flex-end';
+        }
+    
+        btnClickHide.innerHTML = `收起議題 <img class="arrow" alt="up arrow" src="img/up-arrow.png">`;
+        isClicked = true;
+    } else {
+        for (let node of hide){
+            node.style.display = 'none';
+        }
+
+        btnClickHide.innerHTML = `看更多議題 <img class="arrow" alt="arrow" src="img/right-arrow.png">`;
+        isClicked = false;
+    }
+    
+})
