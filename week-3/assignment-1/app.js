08622c372e3d056016408e4106a9a494c62a39e0
Po Lien Lin <polien.joe.lin@gmail.com>
2023-09-16 22:50:46 +0800

    Add error handler

--- week-3/assignment-1/app.js
+++ week-3/assignment-1/app.js
@@ -17,6 +17,21 @@ app.use(mainRoutes);
 //  Add static files
 app.use('/', express.static('public'));
 
+//  Add error handler
+app.use((req, res, next) => {
+	const err = new Error('Not Found');
+    console.log('Error Handler is being executed.');
+	err.status = 404;
+	next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error.pug');
+})
+
+
 // Set Server listener
 app.listen(3000, ()=> {
     console.log("The application is running on localhost: 3000")
