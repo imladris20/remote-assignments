7173b6dbafbed6ac89c1b8fda6e7a9e6b12d1589
Po Lien Lin <polien.joe.lin@gmail.com>
2023-09-16 20:35:17 +0800

    Finish assignment 2

--- week-3/assignment-1/routes/index.js
+++ week-3/assignment-1/routes/index.js
@@ -6,7 +6,31 @@ router.get('/', (req, res) => {
 })
 
 router.get('/getData', (req,res) => {
-    res.render('getData.pug');
+    const query = req.query;
+    let number = query.number;
+    
+    if(isNaN(number)){
+        number = '';
+    } else {
+        number = parseInt(number);
+    }
+    console.log(`The type of number is ${typeof number} and its value is ${number}.`)
+
+    let sum = 0;
+    let countingDescription = '';
+    for(i=1; i<=number ; i++){
+        sum += i;
+        if (i<number){
+            countingDescription += `${i} + `;
+        } else {
+            countingDescription += `${i} = ${sum}`;
+        }
+    }
+
+    console.log("Sum is: ", sum);
+    console.log("Result is: ", countingDescription);
+
+    res.render('getData.pug', {query, sum, countingDescription, number});
 })
 
 module.exports = router;
