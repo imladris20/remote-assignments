863949bea6479010d0cee4805566cb4d60e24b68
Po Lien Lin <polien.joe.lin@gmail.com>
2023-09-24 22:05:48 +0800

    retry

--- week-4/assignment3/src/main.jsx
+++ week-4/assignment3/src/main.jsx
@@ -8,7 +8,7 @@ import { DataProvider } from './context/DataContext.jsx'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/remote-assignments/week-4/assignment3">
+    <BrowserRouter>
       <DataProvider>
         <App />
       </DataProvider>
