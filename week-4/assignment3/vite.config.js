6580dccd07e895597d7f75147868e59f4f74c41b
Po Lien Lin <polien.joe.lin@gmail.com>
2023-09-24 21:44:51 +0800

    Fix the path

--- week-4/assignment3/vite.config.js
+++ week-4/assignment3/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/remote-assignments/week-4/assignment3/",
+  base: "/remote-assignments/week-4/assignment3",
   plugins: [react()],
 })
