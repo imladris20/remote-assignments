341181fcf1aeb02649aeab5c4cf5bd4d0f7fb3b2
Po Lien Lin <polien.joe.lin@gmail.com>
2023-09-24 16:51:14 +0800

    Set deployment

--- week-4/assignment3/src/main.jsx
+++ week-4/assignment3/src/main.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { DataProvider } from './context/DataContext.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter basename="/remote-assignments/week-4/assignment3/">
       <DataProvider>
         <App />
       </DataProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
