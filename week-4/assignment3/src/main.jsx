d6cfd6bffeb05d8f273301c1e4bf7e6f00dd1b16
Po Lien Lin <polien.joe.lin@gmail.com>
2023-09-24 18:08:41 +0800

    Fix the bugs

--- week-4/assignment3/src/main.jsx
+++ week-4/assignment3/src/main.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { DataProvider } from './context/DataContext.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/remote-assignments/week-4/assignment3/">
+    <BrowserRouter>
       <DataProvider>
         <App />
       </DataProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
