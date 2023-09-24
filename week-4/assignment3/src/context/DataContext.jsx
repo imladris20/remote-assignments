ddbe6c8b48fadc7519f4756051f9587979e9ae97
Po Lien Lin <polien.joe.lin@gmail.com>
2023-09-24 18:20:20 +0800

    Deploy again

--- week-4/assignment3/src/context/DataContext.jsx
+++ week-4/assignment3/src/context/DataContext.jsx
@@ -1,23 +1,10 @@
-import {createContext} from "react";
+import { createContext } from "react";
 import { Issues } from '../assets/issues'
 import { useState } from "react";
 
 const DataContext = createContext(null);
 
 export const DataProvider = (props) => {
-  // const [user, setUser] = useState(null);
-
-  // const signInUser = (username, password) => {
-  //   const newUser = {
-  //     username,
-  //     password
-  //   };
-  //   setUser(newUser);
-  // }
-
-  // const signOutUser = () => {
-  //   setUser(null);
-  // }
 
   const [isChanged, setChanging] = useState(false);
   const [isOriginNavShowed, setOrigin] = useState(true);
