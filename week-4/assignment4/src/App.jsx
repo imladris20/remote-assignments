e3ecfcdc3b2c90a04f3976657c12a3194fba1c40
Po Lien Lin <polien.joe.lin@gmail.com>
2023-09-27 14:47:51 +0800

    working on progress of destructuring fetched data

--- week-4/assignment4/src/App.jsx
+++ week-4/assignment4/src/App.jsx
@@ -1,10 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
 import fbLogo from '/facebook-48.svg'
 import './App.css'
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  let [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.github.com/orgs/facebook/repos?per_page=5&page=1')
+      .then(res => {
+        console.log(res.data[0]);
+        setData(res.data.map( item => {
+          return {
+            name: item.name,
+            description: item.description,
+            visibility: item.visibility,
+            topics: item.topics            
+          }
+        }))
+      }) 
+      .catch( error => console.log("Something went wrong.", error))
+      .finally( () => console.log("It's done!"))
+
+  }, [])
+
+  console.log(data);
+
+  const initialData = data.map( item => {
+    return(
+      <div>
+        <h1>{item.name}</h1>
+        <h2>{item.description}</h2>
+      </div>
+    )
+  })
 
   return (
     <>
@@ -14,6 +44,7 @@ function App() {
         </a>
       </div>
       <h1>Week-4 Assignment4</h1>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           More {count}
